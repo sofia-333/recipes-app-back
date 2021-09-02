7d82620756a1213bbad138a5d7a45d6b5d75ee78
Sofia Muradovi <sofio.muradovi@gmail.com>
2021-09-02 13:44:45 +0400

    Implement calculate_time function based view to calculate time to prepare for several recipes.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,13 @@
+from django.urls import path
 from rest_framework import routers
-from .views import RecipeViewSet
+from .views import RecipeViewSet, calculate_time
 
 app_name = 'recipes'
 
 router = routers.DefaultRouter()
 router.register('api/recipes', RecipeViewSet, 'recipes')
 
-urlpatterns = router.urls
+urlpatterns = [
+                  path('api/recipes/calculate/', calculate_time, name='calculate-time')
+               ] + router.urls
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,7 @@
+import json
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Recipe
 from rest_framework import viewsets, status
@@ -11,3 +14,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filter_class = RecipeFilter
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+@api_view(['POST'])
+def calculate_time(request):
+    recipes = Recipe.objects.filter(id__in=request.data)
+    recipes_times = [r.time_to_prepare for r in recipes]
+    time = sum(recipes_times)
+    return Response(json.dumps(time), status=status.HTTP_200_OK)
