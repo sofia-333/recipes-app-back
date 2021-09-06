f75f968ead6f48b7728612f01a94698e52d387a6
Sofia Muradovi <sofio.muradovi@gmail.com>
2021-09-06 15:10:11 +0400

    Add max_time method filter.

--- recipes/filters.py
+++ recipes/filters.py
@@ -4,7 +4,13 @@ from .models import Recipe
 
 class RecipeFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='contains')
+    time_to_prepare = django_filters.CharFilter(method='max_time')
 
     class Meta:
         model = Recipe
         fields = ['title']
+
+    @staticmethod
+    def max_time(queryset, name, value):
+        queryset = queryset.filter(time_to_prepare__lt=value)
+        return queryset
